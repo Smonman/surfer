1d358a5134d643e0b43ea07a3d7156915a9c7dd2
Simon Josef Kreuzpointner <e12021358@student.tuwien.ac.at>
2025-08-22 13:59:08 +0200

    feat: make input arguments mutually exclusive

--- src/surfer.py
+++ src/surfer.py
@@ -20,8 +20,9 @@ def main(args: dict) -> None:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Send images to a waveshare e-ink display.")
-    parser.add_argument("image", type=pathlib.Path, nargs="?", help="Image to display")
-    parser.add_argument("-w", "--watch-directory", type=pathlib.Path, help="watch directory and display lates changes")
+    input_group = parser.add_mutually_exclusive_group()
+    input_group.add_argument("image", type=pathlib.Path, nargs="?", help="Image to display")
+    input_group.add_argument("-w", "--watch-directory", type=pathlib.Path, help="watch directory and display lates changes")
     parser.add_argument("-v", "--verbose", action="store_true", default=False, help="show more log output")
     parser.add_argument("-e", "--debug", action="store_true", default=False, help="show debug log messages")
     args = parser.parse_args()
